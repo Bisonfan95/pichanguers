5076e177973ec5e7ad38525dc726967d1095eca3
W3234 <W3234@lab.aca>
2016-05-11 07:51:30 -0500

    ..

--- app/src/main/java/login/LoginActivity.java
+++ app/src/main/java/login/LoginActivity.java
@@ -3,5 +3,11 @@ package login;
 /**
  * Created by w3234 on 11/05/2016.
  */
-public class LoginActivity {
+public class LoginActivity implements LoginView{
+
+    LoginPresenter loginPresenter;
+    @Override
+    public void setPresenter(LoginPresenter login) {
+
+    }
 }
